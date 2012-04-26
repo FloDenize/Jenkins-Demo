4198b4a9d34a57cac6312afcb8eedf6e466fb5d1
FloDenize <florimond.denize@gmail.com>
2012-04-26 22:48:49 +0300

    Update test/SmokeTest.java

--- test/SmokeTest.java
+++ test/SmokeTest.java
@@ -1,28 +1,38 @@
+
 import com.thoughtworks.selenium.*;
+import java.util.regex.Pattern;
 import org.junit.*;
 import com.testingbot.*;
 
-public class SmokeTest extends TestingBotTestCase {
-  public void setUp() throws Exception {
-    TestingBotSelenium selenium = new TestingBotSelenium(
-            "hub.testingbot.com",
-            4444,
-            "firefox",
-            "http://www.google.com/");
+public class TestLogin extends TestingBotTestCase {
 
-    this.selenium = selenium;
-    selenium.start("version=10;platform=WINDOWS;screenshot=false");
-    
-    // print sessionID in output so that our Jenkins plugin maps the sessionID to videos/screenshots
-    System.out.println("TestingBotSessionID=" + this.selenium.getEval("selenium.sessionId"));
-  }
+    @Override
+    public void setUp() throws Exception {
+        TestingBotSelenium selenium = new TestingBotSelenium(
+                "hub.testingbot.com",
+                4444,
+                "firefox",
+                "http://pp.myerp.com");
+        selenium.start("version=10;platform=WINDOWS;screenshot=true;screenrecorder=false");
+        this.selenium = selenium;
+    }
 
-  public void testGoogle() throws Exception {
-    this.selenium.open("/");
-    assertEquals("Google", this.selenium.getTitle());
-  }
+    public void testLogin() throws Exception {
+        // Opening Login :
+        selenium.open("/index.jsp?locale=en_US");
+        // Fill Username :
+        selenium.type("//*[@id=\"userUsername\"]", "fxbilloir+2@gmail.com");
+        selenium.fireEvent("//*[@id=\"userUsername\"]", "blur");
+        //Fill Password :
+        selenium.type("//*[@id=\"userPassword\"]", "fxbilloir");
+        selenium.fireEvent("//*[@id=\"userPassword\"]", "blur");
+        Thread.sleep(200);
+        selenium.click("css=input[type=\"image\"]");
+        selenium.waitForPageToLoad("30000");
+    }
 
-  public void tearDown() throws Exception {
-    this.selenium.stop();
-  }
+    @Override
+    public void tearDown() throws Exception {
+        this.selenium.stop();
+    }
 }
